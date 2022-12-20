cddae6eef7b94614e257c61fdb15a3b5ff6a85e5
김재성 <aamoos@naver.com>
2022-12-20 17:45:59 +0900

    Initial commit

---
+++ src/main/java/com/codingTest/exam0101/Main.java
@@ -0,0 +1,29 @@
+package com.codingTest.exam0101;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine().toLowerCase();
+		char b = br.readLine().toLowerCase().charAt(0);
+		int count = 0;
+
+		//향상된 for문
+		for (char x : a.toCharArray()) {
+			if(b == x){
+				count++;
+			}
+		}
+
+		//for문
+//		for (int i=0; i<a.length(); i++){
+//			if(b == a.charAt(i)){
+//				count++;
+//			}
+//		}
+		System.out.println(count);
+	}
+}
