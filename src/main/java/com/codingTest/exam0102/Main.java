8ea47a66164eb065f5b439863cd144862bcd45d6
김재성 <aamoos@naver.com>
2022-12-21 20:52:42 +0900

    01-02 아스키코드 풀이 추가

--- src/main/java/com/codingTest/exam0102/Main.java
+++ src/main/java/com/codingTest/exam0102/Main.java
@@ -11,13 +11,32 @@ public class Main {
 		String result = "";
 
 		//string for문 돌리기
+//		for (char c : s.toCharArray()) {
+//			if(Character.isUpperCase(c)){
+//				//대문자
+//				result+=Character.toLowerCase(c);
+//			} else if(Character.isLowerCase(c)){
+//				//소문자
+//				result+=Character.toUpperCase(c);
+//			}
+//		}
+		/*
+		아스키 코드
+		대문자 : 65~90
+		소문자 : 97~122  (소문자 32를 뺴면 대문자)
+		*/
+
 		for (char c : s.toCharArray()) {
-			if(Character.isUpperCase(c)){
-				//대문자
-				result+=Character.toLowerCase(c);
-			} else if(Character.isLowerCase(c)){
-				//소문자
-				result+=Character.toUpperCase(c);
+			//소문자일경우
+			if(c>=97 && c<=122){
+				//대문자로변환
+				result+= (char) (c-32);
+			}
+
+			//대문자일경우
+			else{
+				//소문자로변환
+				result+= (char) (c+32);
 			}
 		}
 		System.out.println(result);
