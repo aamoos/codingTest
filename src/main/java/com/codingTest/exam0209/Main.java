f6d76c830c9c5c9fd394479963058bfd5355303a
김재성 <aamoos@naver.com>
2023-01-24 12:57:14 +0900

    강의

---
+++ src/main/java/com/codingTest/exam0209/Main.java
@@ -0,0 +1,44 @@
+package com.codingTest.exam0209;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[][] arr = new int[n][n];
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				arr[i][j] = scan.nextInt();
+			}
+		}
+		System.out.print(solution(n, arr));
+
+	}
+
+	public static int solution(int n, int[][] arr){
+		int answer = Integer.MIN_VALUE;
+		int sum1, sum2;
+		for(int i=0; i<n; i++){
+			sum1=sum2=0;
+
+			for(int j=0; j<n; j++){
+				sum1+=arr[i][j];
+				sum2+=arr[j][i];
+			}
+			answer=Math.max(answer, sum1);
+			answer=Math.max(answer, sum2);
+		}
+		sum1=sum2=0;
+		for(int i=0; i<n; i++){
+			sum1+=arr[i][i];
+			sum2+=arr[i][n-i-1];
+		}
+		answer=Math.max(answer, sum1);
+		answer=Math.max(answer, sum2);
+
+		return answer;
+	}
+}
---
+++ src/main/java/com/codingTest/exam0211/Main.java
@@ -0,0 +1,41 @@
+package com.codingTest.exam0211;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[][] arr = new int[n+1][6];
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=5; j++){
+				arr[i][j] = scan.nextInt();
+			}
+		}
+		System.out.println(solution(n, arr));
+
+	}
+
+	public static int solution(int n, int[][] arr){
+		int answer = 0, max = Integer.MIN_VALUE;
+
+		for(int i=1; i<=n; i++){
+			int cnt = 0;
+			for(int j=1; j<=n; j++){
+				for(int k=1; k<=5; k++){
+					if(arr[i][k] == arr[j][k]){
+						cnt++;
+						break;
+					}
+				}
+			}
+			if(cnt>max){
+				max=cnt;
+				answer=i;
+			}
+		}
+		return answer;
+	}
+}
