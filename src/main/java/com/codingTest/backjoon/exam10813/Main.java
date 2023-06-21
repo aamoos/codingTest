6b3d5db591515bf3cf893bcc4b3a441778a8ef45
kimjaesung <aamoos@naver.com>
2023-06-21 09:52:19 +0900

    코딩테스트

---
+++ src/main/java/com/codingTest/backjoon/exam10813/Main.java
@@ -0,0 +1,51 @@
+package com.codingTest.backjoon.exam10813;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : com.codingTest.backjoon.exam10813
+ * fileName       : Main
+ * author         : 김재성
+ * date           : 2023-06-21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-21        김재성       최초 생성
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        int M = Integer.parseInt(st.nextToken());
+        int temp;
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = i+1;
+        }
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int I = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+
+            temp = arr[I-1];
+            arr[I-1] = arr[J-1];
+            arr[J-1] = temp;
+        }
+
+        for(int k=0; k<arr.length; k++){
+            bw.write(arr[k] + " ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
