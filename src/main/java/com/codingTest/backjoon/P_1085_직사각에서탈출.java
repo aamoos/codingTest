44a130337a7940c34afcbfe2f857bac2c23c77d6
kimjaesung <aamoos@naver.com>
2023-09-18 10:59:20 +0900

    백준

---
+++ src/main/java/com/codingTest/backjoon/P_1085_직사각에서탈출.java
@@ -0,0 +1,30 @@
+package com.codingTest.backjoon;
+
+import java.util.Scanner;
+
+/**
+ * packageName    : com.codingTest.backjoon
+ * fileName       : P_1085_직사각에서탈출
+ * author         : 김재성
+ * date           : 2023-09-18
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-09-18        김재성       최초 생성
+ */
+public class P_1085_직사각에서탈출 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int w = scan.nextInt();
+        int h = scan.nextInt();
+
+        int x_min = Math.min(x, w-x);
+        int y_min = Math.min(y, h-y);
+
+        System.out.println(Math.min(x_min, y_min));
+    }
+}
---
+++ src/main/java/com/codingTest/backjoon/P_27323_직사각형.java
@@ -0,0 +1,23 @@
+package com.codingTest.backjoon;
+
+import java.util.Scanner;
+
+/**
+ * packageName    : com.codingTest.backjoon
+ * fileName       : P_27323_직사각형
+ * author         : 김재성
+ * date           : 2023-09-18
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-09-18        김재성       최초 생성
+ */
+public class P_27323_직사각형 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A*B);
+    }
+}
