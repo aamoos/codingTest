ade96782c87f48e3c9ea97f6973e156502c3ef3a
김재성 <aamoos@naver.com>
2023-09-21 10:07:33 +0900

    백준 문제풀이

---
+++ src/main/java/com/codingTest/backjoon/P2745_진법변환.java
@@ -0,0 +1,27 @@
+package com.codingTest.backjoon;
+
+import java.util.Scanner;
+
+public class P2745_진법변환 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+        int N = scan.nextInt();
+        int tmp = 1;
+        int ans = 0;
+
+        for(int i = s.length()-1; i>=0; i--){
+            char c = s.charAt(i);
+
+            if('A' <= c && c <= 'Z'){
+                ans += (c-'A'+10) * tmp;
+            }else{
+                ans += (c-'0') * tmp;
+            }
+            tmp *= N;
+        }
+        System.out.println(ans);
+    }
+}
