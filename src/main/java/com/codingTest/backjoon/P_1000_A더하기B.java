816523a32b2c923a1c9d3a9acfa68400b7e43ff5
김재성 <aamoos@naver.com>
2023-09-21 11:17:04 +0900

    백준

---
+++ src/main/java/com/codingTest/backjoon/P_1000_A더하기B.java
@@ -0,0 +1,16 @@
+package com.codingTest.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_1000_A더하기B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.print(A+B);
+    }
+}
---
+++ src/main/java/com/codingTest/backjoon/P_2557_HelloWorld.java
@@ -0,0 +1,7 @@
+package com.codingTest.backjoon;
+
+public class P_2557_HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
