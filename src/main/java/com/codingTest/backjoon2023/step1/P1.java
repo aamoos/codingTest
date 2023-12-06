d1914fc4eb80a9942f1e2d330817bb37e5e5f45d
KimJaeSung <aamoos@naver.com>
2023-12-06 10:37:49 +0900

    2023 알고리즘 시작

---
+++ src/main/java/com/codingTest/backjoon2023/step1/P1.java
@@ -0,0 +1,18 @@
+package com.codingTest.backjoon2023.step1;
+
+/**
+ * packageName    : com.codingTest.backjoon2023.step1
+ * fileName       : P1
+ * author         : 김재성
+ * date           : 2023-12-06
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-12-06        김재성       최초 생성
+ */
+public class P1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/codingTest/backjoon2023/step1/P2.java
@@ -0,0 +1,27 @@
+package com.codingTest.backjoon2023.step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : com.codingTest.backjoon2023.step1
+ * fileName       : P2
+ * author         : 김재성
+ * date           : 2023-12-06
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-12-06        김재성       최초 생성
+ */
+public class P2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
+    }
+}
