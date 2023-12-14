14d97dff73436e53cdc962b36fb7b27ee517d418
김재성 <aamoos@naver.com>
2023-12-14 10:06:44 +0900

    백준 단계별로 풀기

---
+++ src/main/java/com/codingTest/backjoon2023/step1/P12.java
@@ -0,0 +1,10 @@
+package com.codingTest.backjoon2023.step1;
+
+public class P12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
