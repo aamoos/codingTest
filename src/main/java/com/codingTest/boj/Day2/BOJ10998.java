32cf662e183a7c60f28d66e02ac1693290919855
김재성 <aamoos@naver.com>
2024-01-03 19:43:00 +0900

    백준 2일차

---
+++ src/main/java/com/codingTest/boj/Day2/BOJ10998.java
@@ -0,0 +1,12 @@
+package com.codingTest.boj.Day2;
+
+import java.util.Scanner;
+
+public class BOJ10998 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A*B);
+    }
+}
