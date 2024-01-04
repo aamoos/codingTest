55447a0cad3382b29b9723bd718678fff7f07e42
김재성 <aamoos@naver.com>
2024-01-04 14:46:55 +0900

    백준 3일차

---
+++ src/main/java/com/codingTest/boj/Day3/BOJ2438.java
@@ -0,0 +1,17 @@
+package com.codingTest.boj.Day3;
+
+import java.util.Scanner;
+
+public class BOJ2438 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/com/codingTest/boj/Day3/BOJ2739.java
@@ -0,0 +1,13 @@
+package com.codingTest.boj.Day3;
+
+import java.util.Scanner;
+
+public class BOJ2739 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i=1; i<=9; i++){
+            System.out.println(N+" * "+i+" = "+(N*i));
+        }
+    }
+}
