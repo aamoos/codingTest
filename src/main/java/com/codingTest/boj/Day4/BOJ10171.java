3e98e4708805084c0790672a17a0a431592b8f22
aamoos <aamoos@naver.com>
2024-01-16 14:22:42 +0900

    백준 단계별로 풀기

---
+++ src/main/java/com/codingTest/boj/Day4/BOJ10171.java
@@ -0,0 +1,10 @@
+package com.codingTest.boj.Day4;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
