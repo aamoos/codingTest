b5b8a7ffb806be6fa62c466c97b9d71d8f861d82
김재성 <aamoos@openmate.co.kr>
2024-04-01 18:23:17 +0900

    공부

---
+++ src/main/java/com/codingTest/알고리즘2024/chap1/sector1/practice3.java
@@ -0,0 +1,28 @@
+package com.codingTest.알고리즘2024.chap1.sector1;
+
+import java.util.Scanner;
+
+public class practice3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String str){
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+
+        String[] arr = str.split(" ");
+
+        for (String s : arr) {
+            int len = s.length();
+            if(len > m){
+                m = len;
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+}
