77ff3c020df2a2af4624524138bd92aef964cde7
김재성 <aamoos@openmate.co.kr>
2024-04-03 09:52:51 +0900

    공부

---
+++ src/main/java/com/codingTest/알고리즘2024/chap1/sector1/practice4.java
@@ -0,0 +1,33 @@
+package com.codingTest.알고리즘2024.chap1.sector1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class practice4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] str = new String[n];
+
+        for(int i=0; i<n; i++){
+            str[i] = scan.next();
+        }
+
+        for(String x : solution(n, str)){
+            System.out.println(x);
+        }
+
+    }
+
+    public static ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+}
