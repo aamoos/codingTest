9bac198120e15e014646cbdf94d5380b635f0cf3
김재성 <aamoos@openmate.co.kr>
2024-06-14 13:08:26 +0900

    [배열과 리스트 실전 문제] 평균 구하기 (백준 1546)

--- src/main/java/com/codingTest/book/P1546_평균구하기.java
+++ src/main/java/com/codingTest/book/P1546_평균구하기.java
@@ -8,18 +8,16 @@ public class P1546_평균구하기 {
         int N = scan.nextInt();
         int[] A = new int[N];
 
-        for(int i=0; i<N; i++){
-            A[i] = scan.nextInt();
-        }
-
         long sum = 0;
         long max = 0;
 
         for(int i=0; i<N; i++){
-           if(A[i] > max){
-               max = A[i];
-           }
-           sum = sum + A[i];
+            int temp = scan.nextInt();
+
+            if(temp > max){
+               max = temp;
+            }
+            sum = sum + temp;
         }
 
         System.out.println(sum*100.0/max/N);
